e31f8d9b7772e1c804789a9723c8dd828b0c569f
Scott Lagler <laglerscott@gmail.com>
2023-05-11 20:02:46 -0500

    feat: add argument parsing

--- SaveHeaders.py
+++ SaveHeaders.py
@@ -3,21 +3,7 @@ import argparse
 import os
 import sys
 
-SYSNAME = os.uname().sysname
-
-def get_sys_include_dirs() -> List[str]:
-	if SYSNAME == "Darwin":
-		include_dirs = ["/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/14.0.3/include", "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include", "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include", "/usr/local/include"]
-		# os.popen("clang -Wp,-v -E -")
-		# Look at how to read stderr from a subprocess
-		return include_dirs
-	else:
-		print(f"[!] Unsupported OS: {SYSNAME}")
-		sys.exit(0)
-
-
 def parse_header(bv: BinaryView, header_path: str, options: List[str], include_dirs: List[str]) -> bool:
-	include_dirs += get_sys_include_dirs()
 	with open(header_path) as header_str:
 		try:
 			result = bv.parse_types_from_string(header_str.read(), options, include_dirs)
@@ -49,10 +35,21 @@ def parse_header(bv: BinaryView, header_path: str, options: List[str], include_d
 		# add variables
 	return True
 
-include_dirs = get_sys_include_dirs()
-bv = open_view("./megatest/build/megatest")
-parse_header(bv, "./megatest/src/store.h", [], include_dirs)
-parse_header(bv, "./megatest/src/megatest.h", [], include_dirs)
+#include_dirs = get_sys_include_dirs()
+parser = argparse.ArgumentParser(prog="HeadlessHeaders", description="Headless C/C++ header parser for binary ninja", epilog="Author: @SmoothHacker")
+parser.add_argument('-bv', '--binaryview', dest="user_binaryview")
+parser.add_argument('-i', '--include-dirs', dest="include_dirs", default=[""])
+parser.add_argument('--options', dest="options", default=[""])
+parser.add_argument('headers', nargs='+')
+
+args = parser.parse_args()
+
+bv = open_view(os.path.abspath(args.user_binaryview))
+include_dirs = args.include_dirs.split(',')
+include_dirs = [os.path.abspath(x) for x in include_dirs]
+
+for header in args.headers:
+	parse_header(bv, header, args.options, include_dirs)
 
 print(f"[*] Saving to -> {os.path.basename(bv.file.filename)}.bndb")
 bv.create_database(f"{os.path.basename(bv.file.filename)}.bndb")
